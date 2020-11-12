32dad4c0fb8288a54422681b6739f7ddaf04b97f
like <like.ren@sjtu.edu.cn>
2020-11-12 23:10:38 +0800

    have a try

---
+++ cmake-build-debug/CMakeFiles/mycpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mycpp.dir/main.cpp.obj"
+  "libmycpp.dll.a"
+  "mycpp.exe"
+  "mycpp.exe.manifest"
+  "mycpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mycpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
